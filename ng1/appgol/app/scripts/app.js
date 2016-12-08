f114b9d1f88c61854ec2a9727aee0af552598525
Jorge Arevalo <jorgeas80@gmail.com>
2016-12-08 23:52:33 +0100

    Added ng1 game of life app, using old syntax

---
+++ ng1/appgol/app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name appgolApp
+ * @description
+ * # appgolApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('appgolApp', [
+    'ngResource'
+  ]);
